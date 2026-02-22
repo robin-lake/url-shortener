c1fef86e66341f717112b9aafe910c9eab9f2277
Lily Lake <robinlake@Lilypad.local>
2026-02-22 10:52:09 -0800

    blank vite project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.scss'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="body">
+      <h1>New App</h1>
+    </div>
+  )
+}
+
+export default App
